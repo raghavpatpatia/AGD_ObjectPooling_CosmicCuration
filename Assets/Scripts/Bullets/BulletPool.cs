4614ef9669d9c723ddaf7ec94aafd7daa7deb71f
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-03-02 01:32:15 +0530

    Cosmic Curation
    Created BulletPool class

---
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace CosmicCuration.Bullets
+{
+    public class BulletPool : MonoBehaviour
+    {
+        private BulletView bulletView;
+        private BulletScriptableObject bulletScriptableObject;
+        List<PooledBullet> pooledBullet = new List<PooledBullet>();
+        public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
+        {
+            this.bulletView = bulletView;
+            this.bulletScriptableObject = bulletScriptableObject;
+        }
+    }
+    public class PooledBullet
+    {
+        public BulletController bulletController;
+        public bool isUsed;
+    }
+}
