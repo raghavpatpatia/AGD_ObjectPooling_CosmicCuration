977c843ed3b5c45f323f7e33c3d5331c46c085d5
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-03-02 15:04:33 +0530

    Cosmic Curation
    Created GetBullet Method

--- Assets/Scripts/Bullets/BulletPool.cs
+++ Assets/Scripts/Bullets/BulletPool.cs
@@ -3,17 +3,39 @@ using UnityEngine;
 
 namespace CosmicCuration.Bullets
 {
-    public class BulletPool : MonoBehaviour
+    public class BulletPool
     {
         private BulletView bulletView;
         private BulletScriptableObject bulletScriptableObject;
-        List<PooledBullet> pooledBullet = new List<PooledBullet>();
+        List<PooledBullet> pooledBullets = new List<PooledBullet>();
         public BulletPool(BulletView bulletView, BulletScriptableObject bulletScriptableObject)
         {
             this.bulletView = bulletView;
             this.bulletScriptableObject = bulletScriptableObject;
         }
+        public BulletController GetBullet()
+        {
+            if (pooledBullets.Count > 0)
+            {
+                PooledBullet pooledBullet = pooledBullets.Find(bullet => !bullet.isUsed);
+                if (pooledBullet != null)
+                {
+                    pooledBullet.isUsed = true;
+                    return pooledBullet.bulletController;
+                }
+            }
+            return CreateBullet();
+        }
+        private BulletController CreateBullet()
+        {
+            PooledBullet bullet = new PooledBullet();
+            bullet.isUsed = true;
+            bullet.bulletController = new BulletController(bulletView, bulletScriptableObject);
+            pooledBullets.Add(bullet);
+            return bullet.bulletController;
+        }
     }
+
     public class PooledBullet
     {
         public BulletController bulletController;
