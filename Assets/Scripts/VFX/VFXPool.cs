0b449dfad361d65dc52b5414b6c3f5db6736505d
Raghav Patpatia <raghav.patpatia@gmail.com>
2024-03-05 19:14:08 +0530

    Cosmic Curation
    Added PowerUpPool.

---
+++ Assets/Scripts/VFX/VFXPool.cs
@@ -0,0 +1,15 @@
+﻿using CosmicCuration.PowerUps;
+using CosmicCuration.Utilities;
+
+namespace CosmicCuration.VFX
+{
+    public class VFXPool : GenericObjectPool<VFXController>
+    {
+        private VFXView vfxView;
+        public VFXController GetVFX<T>(VFXView vfxView) where T : VFXController
+        {
+            this.vfxView = vfxView;
+            return GetItem<T>();
+        }
+    }
+}
